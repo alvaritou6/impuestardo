34d01f603c6286119e8a3788c4ba474474823509
Alvaro Riba <alvaroriba585@gmail.com>
2024-02-23 12:39:45 -0300

    v1

---
+++ main.py
@@ -0,0 +1,95 @@
+from time import sleep
+import requests
+import os
+import json
+
+# obtención de la cotización actual del dolar
+response = requests.get('https://api.bluelytics.com.ar/v2/latest').text
+json_dls = json.loads(response)
+dls_oficial = json_dls["oficial"]
+dolar = dls_oficial["value_sell"]
+
+# creación de las variables para un control más efectivo de los impuestos
+imp_iva = 21
+imp_pais = 8
+imp_ganancias = 30
+
+# bienvenida a la app al usuario
+print("""
+ ██▓ ███▄ ▄███▓ ██▓███   █    ██ ▓█████   ██████ ▄▄▄█████▓ ▄▄▄       ██▀███  ▓█████▄  ▒█████  
+▓██▒▓██▒▀█▀ ██▒▓██░  ██▒ ██  ▓██▒▓█   ▀ ▒██    ▒ ▓  ██▒ ▓▒▒████▄    ▓██ ▒ ██▒▒██▀ ██▌▒██▒  ██▒
+▒██▒▓██    ▓██░▓██░ ██▓▒▓██  ▒██░▒███   ░ ▓██▄   ▒ ▓██░ ▒░▒██  ▀█▄  ▓██ ░▄█ ▒░██   █▌▒██░  ██▒
+░██░▒██    ▒██ ▒██▄█▓▒ ▒▓▓█  ░██░▒▓█  ▄   ▒   ██▒░ ▓██▓ ░ ░██▄▄▄▄██ ▒██▀▀█▄  ░▓█▄   ▌▒██   ██░
+░██░▒██▒   ░██▒▒██▒ ░  ░▒▒█████▓ ░▒████▒▒██████▒▒  ▒██▒ ░  ▓█   ▓██▒░██▓ ▒██▒░▒████▓ ░ ████▓▒░
+░▓  ░ ▒░   ░  ░▒▓▒░ ░  ░░▒▓▒ ▒ ▒ ░░ ▒░ ░▒ ▒▓▒ ▒ ░  ▒ ░░    ▒▒   ▓▒█░░ ▒▓ ░▒▓░ ▒▒▓  ▒ ░ ▒░▒░▒░ 
+ ▒ ░░  ░      ░░▒ ░     ░░▒░ ░ ░  ░ ░  ░░ ░▒  ░ ░    ░      ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ▒  ▒   ░ ▒ ▒░ 
+ ▒ ░░      ░   ░░        ░░░ ░ ░    ░   ░  ░  ░    ░        ░   ▒     ░░   ░  ░ ░  ░ ░ ░ ░ ▒  
+ ░         ░               ░        ░  ░      ░                 ░  ░   ░        ░        ░ ░  
+                                                                              ░               
+""")
+print('\n\n\nCargando app...')
+sleep(2)
+os.system("cls")
+
+# calculo en pesos
+def pesos(val_pesos):
+    if val_pesos.isdigit() == True:
+        val_pesos = float(val_pesos)
+        iva = val_pesos*imp_iva/100
+        print(f'IVA (21%): {iva}')
+        pais = val_pesos*imp_pais/100
+        print(f'PAIS (8%): {pais}')
+        ganancias = val_pesos*imp_ganancias/100
+        print(f'Ganancias (30%): {ganancias}')
+        print(f'\nSin impuestos: {val_pesos}')
+        print(f'Total con impuestos: {val_pesos+iva+pais+ganancias}')
+    elif val_pesos == 'EXIT':
+        sleep(1)
+        exit()
+    elif val_pesos == 'DLS':
+        val_dls = input('\n\nIngrese el importe en dolares a cacular impuestos: ')
+        dolares(val_dls)
+    else:
+        print('Error! Escribe un valor válido')
+
+# calculo en dolares
+def dolares(val_dls):
+    if val_dls.isdigit() == True:
+        val_dls = float(val_dls)
+        val_simp = val_dls*dolar
+        iva = val_simp*imp_iva/100
+        print(f'IVA (21%): {iva}')
+        pais = val_simp*imp_pais/100
+        print(f'PAIS (8%): {pais}')
+        ganancias = val_simp*imp_ganancias/100
+        print(f'Ganancias (30%): {ganancias}')
+        print(f'\nSin impuestos: {val_simp}')
+        print(f'Total con impuestos: {val_simp+iva+pais+ganancias}')
+        print(f'\nCotización del dolar: {dolar}')
+    elif val_dls == 'EXIT':
+        sleep(1)
+        exit()
+    elif val_dls == 'ARS':
+        val_pesos = input('\n\nIngrese el importe a calcular impuestos: ')
+        pesos(val_pesos)
+    else:
+        print('Error! Escribe un valor válido')
+
+# elección entre calcular en pesos o dolares
+print('Porfavor elige si quieres calcular impuestos en pesos (ARS) o en dolares (DLS). \nEn el caso elegido escribe TAL CUAL la palabra que está entre parentesis para seguir.')
+print('\nEscriba EXIT para salir del programa.')
+moneda = input('\n--> ')
+
+print('\nSi desea intercambiar entre monedas solamente vuelva a escribir el nombre de la otra moneda TAL CUAL se encuentre entre parentesis.')
+while True:
+    # confección del cálculo en pesos
+    if moneda == 'ARS':
+        val_pesos = input('\n\nIngrese el importe a calcular impuestos: ')
+        pesos(val_pesos)
+    # confección del cálculo en dolares
+    if moneda == 'DLS':
+        val_dls = input('\n\nIngrese el importe en dolares a cacular impuestos: ')
+        dolares(val_dls)
+    if moneda == 'EXIT':
+        sleep(1)
+        exit()
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Impuestardo
+
+Impuestardo is a console application for calculating Argentine taxes on products sold outside the country.
+
+## Instructions for use
+
+1. Run the `main.py` file.
+2. Choose the currency you want to use, Argentine Pesos or Dollars.
+3. If you wanna close the programme type `EXIT`
+4. Enter the amount in the currency of your choice and press ENTER
+5. If you want to change the currency just type the name of the abbreviated currency (ARS or DLS), as you did the first time you entered it. This will result in a new entry where you can put the amount in the changed currency, once the operation is done the program will revert to the previous currency.
+
+</br> </br>
+
+> The dollar quote is taken from [Bluelytics](https://bluelytics.com.ar/#!/). The official dollar rate is taken and 60% tax is added.
+This calculator does not include the IIBB tax as each province has different percentages.
